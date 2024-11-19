c3c0d69a3938acfbc3baa25ecf82512025f6387f
Lucía Carrera <luciaacarreraa@gmail.com>
2024-11-19 22:39:13 +0000

    last challenge doneeee

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  console.log('request body:', JSON.stringify(req.body))
+  next();
+});
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+    res.json({
+      name: req.file.originalname, 
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+  }
+)
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
